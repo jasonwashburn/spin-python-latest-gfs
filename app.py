b2f8513c7ebac4739fe7dda46b455be82a953742
Jason Washburn <jason.washburn@gmail.com>
2023-03-31 11:28:16 -0500

    https response from s3

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
-from spin_http import Response
-
+from spin_http import Response, Request, http_send
+import xml.etree.ElementTree as ET
+import datetime as dt
 
 def handle_request(request):
 
+    response_str = try_https()
+
     return Response(200,
                     [("content-type", "text/plain")],
-                    bytes(f"Hello from the Python SDK", "utf-8"))
+                    bytes(response_str, "utf-8"))
+
+
+def try_https() -> str:
+    response = http_send(Request("GET", "https://noaa-gfs-bdp-pds.s3.amazonaws.com/?list-type=2&prefix=gfs.20230331/00/atmos/gfs.t00z.pgrb2.0p25.f&delimiter=/",[],None))
+     
+    tree = ET.fromstring(response.body)
+    response = [thing for thing in tree.iter("Key")]
+    return f"{response}"
+
+    
